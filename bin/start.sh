5af25715b00d0e468edfc81eb4a5ad36a23654b8
Marcus Ataide <marckaraujo86@gmail.com>
2017-01-30 13:37:21 -0200

    Fix start

--- bin/start.sh
+++ bin/start.sh
@@ -1,2 +1,2 @@
 source /home/ec2-user/mail/.bash_profile
-node /home/ec2-user/mail/lib/server.js > /dev/null 2> /dev/null < /dev/null &
+node /home/ec2-user/mail/npm start > /dev/null 2> /dev/null < /dev/null &
