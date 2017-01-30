9b80421722db86439052769526478a826ea3def9
Marcus Ataide <marckaraujo86@gmail.com>
2017-01-30 14:55:21 -0200

    Fix scripts

--- bin/afterinstall.sh
+++ bin/afterinstall.sh
@@ -1,4 +1,7 @@
 source /home/ec2-user/pro-mail/.bash_profile
 cd /home/ec2-user/mail
+npm update
 npm install
+node node_modules/node-sass/scripts/install.js
+npm rebuild node-sass
 npm install -g babel-cli
--- bin/start.sh
+++ bin/start.sh
@@ -1,2 +1,4 @@
 source /home/ec2-user/mail/.bash_profile
-node /home/ec2-user/mail/npm start > /dev/null 2> /dev/null < /dev/null &
+npm run build
+npm run deploy
+npm run start:prod
